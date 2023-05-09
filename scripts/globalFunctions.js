5f9ce3027ab5c19e91b90b5465b320498f44f1bd
Alex <alexcmilton@gmail.com>
2023-05-10 09:59:41 +1000

    added some missing symbols

--- scripts/globalFunctions.js
+++ scripts/globalFunctions.js
@@ -1,4 +1,4 @@
 export const detectTranslation = (input) => {
-    const checkChar = new RegExp(/[A-Za-z0-9!-'/]/g);
+    const checkChar = new RegExp(/[A-Za-z0-9!-':-@/]/g);
     return checkChar.test(input);
 };
